642a0121fb61d5a482c8a406b31cd2a560d36ecb
gerardodwyer <gerard.odwyer1@gmail.com>
2020-01-29 17:28:43 +0000

    Inital Commit

---
+++ app/src/main/java/com/example/writerschain/adapters/chainAdapter.java
@@ -0,0 +1,4 @@
+package com.example.writerschain.adapters;
+
+public class chainAdapter {
+}
---
+++ app/src/main/java/com/example/writerschain/constructors/Chain.java
@@ -0,0 +1,11 @@
+package com.example.writerschain.constructors;
+
+public class Chain {
+
+    private String chainName;
+    private String authorName;
+    private String chainDescription;
+    private String dateCreated;
+    private String genre;
+    private int chapterCount;
+}
